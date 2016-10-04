469f99391b597c654093d5e77b2a5fbdbc18be32
VampireSH <sherlockya@gmail.com>
2016-10-04 20:07:06 +0800

    2.2

---
+++ 2.2/carrots.cpp
@@ -0,0 +1,18 @@
+// carots.cpp -- food processing program
+// uses and displays a variable
+
+#include <iostream>
+int main(void)
+{
+	using namespace std;
+
+	int carrots;            // declare an integer variable
+
+	carrots = 25;           // assign a value to the variable
+	cout << "I have ";
+	cout << carrots;
+	cout << " carrots." << endl;
+	carrots = carrots - 1; // modify the variable
+	cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	return 0;
+}
