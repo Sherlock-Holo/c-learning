4a7dc9107943e8a560938eef357f84c7385b2ca8
VampireSH <sherlockya@gmail.com>
2016-10-05 16:13:00 +0800

    try to use sqrt()

---
+++ 2.4/sqrt.cpp
@@ -0,0 +1,18 @@
+// sqrt.cpp       using the sqrt()
+
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+	using namespace std;
+	double area;
+	cout << "Enter the floor area, in square feet, of your home: ";
+	cin >> area;
+	double side;
+	side = sqrt(area);
+	cout << "That's the equivalent of a square " << side;
+	cout << " feet to the side." << endl;
+	cout << "How fascinating!" << endl;
+	return 0;
+}
