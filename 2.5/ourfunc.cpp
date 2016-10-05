f46238c711fb56d9afa391bf5cb55594704d11ea
VampireSH <sherlockya@gmail.com>
2016-10-05 17:20:11 +0800

    ourfunc.cpp

---
+++ 2.5/ourfunc.cpp
@@ -0,0 +1,10 @@
+//ourfunc.cpp      defining your own function
+
+#include <iostream>
+void simon(int);                                  // function prototype for simon()
+
+int main(void)
+{
+	using namespace std;
+	simon(3);                                 // call the simon() function
+	cout << "Pick an integer
