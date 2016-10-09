0b7cea1489b8bffba7474409c2127c1a5220ba15
VampireSH <sherlockya@gmail.com>
2016-10-09 21:15:04 +0800

    chartype.cpp

---
+++ chapter-3/3.5/chartype.cpp
@@ -0,0 +1,15 @@
+// chartype.cpp  the char type
+
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+	char ch;                                  // declare a char variable
+
+	cout << "Enter a character: " << endl;
+	cin >> ch;
+	cout << "Hola! ";
+	cout << "Thank you for the " << ch << " character." << endl;
+	return 0;
+}
