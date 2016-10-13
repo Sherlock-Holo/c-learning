eba129c04ff02b6d7b2ed238ba48be838a53dca5
VampireSH <sherlockya@gmail.com>
2016-10-13 11:28:41 +0800

    Escape character

---
+++ chapter-3/3.6/Escape.cpp
@@ -0,0 +1,12 @@
+// Escape ch
+
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+	char alarm = '\a';
+	cout << alarm << "Don't do that again!\a" << endl;
+	cout << "Ben \"Buggsie\" Hacker" << endl << "was here!" << endl;
+	return 0;
+}
